66985f1476a9e8b2c6f3ee648b12a60077970a82
RICHARD NORTH <A0313108@live.tees.ac.uk>
2024-12-04 11:21:29 +0000

    added test failure

---
+++ ThamCoCheapestProductService.Tests/TestFaliure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ThamCoCheapestProductService.Dtos;
+
+namespace ThamCoCheapestProductService.Tests
+{
+    [TestFixture]
+    internal class TestFaliure
+    {
+    }
+
+        [Test]
+        public async Task FailureTest()
+        {
+
+            Assert.AreEqual(0, 1);
+        }
+    }
